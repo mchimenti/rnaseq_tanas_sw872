178106b37bbb87a33abd40c943975fe54b697b75
mchimenti <michael.chimenti@gmail.com>
2019-03-14 14:18:02 -0500

    Update tximport_deseq2_analysis.r
    add DE analysis section; add DEG plots; add volcano plots

--- tximport_deseq2_analysis.r
+++ tximport_deseq2_analysis.r
@@ -116,22 +116,144 @@ pcaExplorer::pcaplot(ddsTxi, intgroup = 'cond', ellipse = FALSE, text_labels = F
 pcaExplorer::pcaplot(rldTxi, intgroup = "cond", ellipse = FALSE, text_labels = FALSE)
 
 plotDispEsts(ddsTxi)
-plotMA(object = ddsTxi, alpha = 0.05)
+plotMA(object = ddsTxi, alpha = 0.05, ylim = c(-10,10))
 
 ##############
 ## DE analysis
 ##############
 
-## Genotype
-res_geno <- results(ddsTxi2, contrast = c("geno","KO","WT"))
-res_geno <- na.omit(res_geno)  #drop NA rows
-res_geno_sig <- res_geno[res_geno$padj < 0.05 & res_geno$baseMean > 5.0,]
-res_geno_ord <- res_geno_sig[order(res_geno_sig$padj),]
-res_geno_ord$ext_gene <- anno[row.names(res_geno_ord), "gene_name"]
+## CAMTA1 vs EV_sub
+res_camta1 <- results(ddsTxi, contrast = c("cond","CAMTA1","EV_sub"))
+res_camta1 <- na.omit(res_camta1)  #drop NA rows
+res_camta1_sig <- res_camta1[res_camta1$padj < 0.05 & res_camta1$baseMean > 5.0,]
+res_camta1_ord <- res_camta1_sig[order(res_camta1_sig$padj),]
+res_camta1_ord$ext_gene <- anno[row.names(res_camta1_ord), "gene_name"]
 
-png("geno_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
-volcanoplot(res_geno_ord, main = "Volcano Plot: DE genes across genotypes, KO vs. WT (Male responses)", lfcthresh=1.0, sigthresh=0.1, textcx=.6, xlim=c(-12, 12), ylim = c(3,25))
+png("camta1_DE_volcano_zoom.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_camta1_ord, main = "Volcano Plot: DE genes across camta1 vs ev_sub", lfcthresh=2, sigthresh=0.05, textcx=.6, xlim=c(-4, 4), ylim = c(3,50))
 dev.off()
 
-degPlot(dds = ddsTxi2, res = res_sex_ord, n = 9, xs = "sex")
+plotCounts(ddsTxi,"ENSG00000128739", intgroup = "cond", normalized = TRUE)
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_camta1_ord, n = 6, group = "cond", 
+        groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+## TAZ4SA vs EV
+res_taz <- results(ddsTxi, contrast = c("cond","TAZ4SA","EV"))
+res_taz <- na.omit(res_taz)  #drop NA rows
+res_taz_sig <- res_taz[res_taz$padj < 0.05 & res_taz$baseMean > 5.0,]
+res_taz_ord <- res_taz_sig[order(res_taz_sig$padj),]
+res_taz_ord$ext_gene <- anno[row.names(res_taz_ord), "gene_name"]
+
+png("taz_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_taz_ord, main = "Volcano Plot: DE genes across taz vs ev", lfcthresh=2, sigthresh=0.05, textcx=.4, xlim=c(-8, 8), ylim = c(3,100))
+dev.off()
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_taz_ord, n = 6, group = "cond", 
+             groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+## TAZ/CAMTA1 vs EV
+res_tc <- results(ddsTxi, contrast = c("cond","TC","EV"))
+res_tc <- na.omit(res_tc)  #drop NA rows
+res_tc_sig <- res_tc[res_tc$padj < 0.05 & res_tc$baseMean > 5.0,]
+res_tc_ord <- res_tc_sig[order(res_tc_sig$padj),]
+res_tc_ord$ext_gene <- anno[row.names(res_tc_ord), "gene_name"]
+
+png("TC_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_tc_ord, main = "Volcano Plot: DE genes across tc vs ev", lfcthresh=2, sigthresh=0.05, textcx=.6, xlim=c(-8, 8), ylim = c(3,120))
+dev.off()
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_tc_ord, n = 6, group = "cond", 
+             groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+
+## TFE3 vs EV
+res_tfe3 <- results(ddsTxi, contrast = c("cond","TFE3","EV"))
+res_tfe3 <- na.omit(res_tfe3)  #drop NA rows
+res_tfe3_sig <- res_tfe3[res_tfe3$padj < 0.05 & res_tfe3$baseMean > 5.0,]
+res_tfe3_ord <- res_tfe3_sig[order(res_tfe3_sig$padj),]
+res_tfe3_ord$ext_gene <- anno[row.names(res_tfe3_ord), "gene_name"]
+
+png("tfe3_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_tfe3_ord, main = "Volcano Plot: DE genes across tfe3 vs ev", lfcthresh=2, sigthresh=0.05, textcx=0.6, xlim=c(-10, 10), ylim = c(3,140))
+dev.off()
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_tfe3_ord, n = 6, group = "cond", 
+             groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+## YAP5SA vs EV
+res_yap5sa <- results(ddsTxi, contrast = c("cond","YAP5SA","EV"))
+res_yap5sa <- na.omit(res_yap5sa)  #drop NA rows
+res_yap5sa_sig <- res_yap5sa[res_yap5sa$padj < 0.05 & res_yap5sa$baseMean > 5.0,]
+res_yap5sa_ord <- res_yap5sa_sig[order(res_yap5sa_sig$padj),]
+res_yap5sa_ord$ext_gene <- anno[row.names(res_yap5sa_ord), "gene_name"]
+
+png("yap5sa_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_yap5sa_ord, main = "Volcano Plot: DE genes across yap5sa vs ev", lfcthresh=2, sigthresh=0.05, textcx=.5, xlim=c(-8, 8), ylim = c(3,120))
+dev.off()
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_yap5sa_ord, n = 6, group = "cond", 
+             groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+## YAP/TFE vs EV
+res_yt <- results(ddsTxi, contrast = c("cond","YT","EV"))
+res_yt <- na.omit(res_yt)  #drop NA rows
+res_yt_sig <- res_yt[res_yt$padj < 0.05 & res_yt$baseMean > 5.0,]
+res_yt_ord <- res_yt_sig[order(res_yt_sig$padj),]
+res_yt_ord$ext_gene <- anno[row.names(res_yt_ord), "gene_name"]
+
+png("YT_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_yt_ord, main = "Volcano Plot: DE genes across YT vs ev", lfcthresh=2, sigthresh=0.05, textcx=.5, xlim=c(-8, 8), ylim = c(3,120))
+dev.off()
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_yt_ord, n = 6, group = "cond", 
+             groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+## TAZ/CAMTA1 vs CAMTA1
+res_tc_camta1 <- results(ddsTxi, contrast = c("cond","tc_camta1","EV"))
+res_tc_camta1 <- na.omit(res_tc_camta1)  #drop NA rows
+res_tc_camta1_sig <- res_tc_camta1[res_tc_camta1$padj < 0.05 & res_tc_camta1$baseMean > 5.0,]
+res_tc_camta1_ord <- res_tc_camta1_sig[order(res_tc_camta1_sig$padj),]
+res_tc_camta1_ord$ext_gene <- anno[row.names(res_tc_camta1_ord), "gene_name"]
+
+png("tc_camta1_DE_volcano.png", 1200, 1500, pointsize=20, res=100)
+volcanoplot(res_tc_camta1_ord, main = "Volcano Plot: DE genes across tc_camta1 vs ev", lfcthresh=2, sigthresh=0.05, textcx=.5, xlim=c(-8, 8), ylim = c(3,120))
+dev.off()
+
+p <- degPlot(ddsTxi, xs = "cond", res = res_tc_camta1_ord, n = 6, group = "cond", 
+             groupLab = "Condition")
+
+p <- p + theme_dark()
+p <- p + theme(axis.text.x = element_text(size=8, angle=45, face="bold"))
+p
+
+
+## TAZ/CAMTA1 vs TAZ
+
+
+
 
